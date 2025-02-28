41cd0b861e96d75171e9f5360033c3a9ee5f8c57
Bruno Fernandes <bruno.enrico99@gmail.com>
2025-02-28 19:22:21 -0300

    Study decision tree completed

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -2,31 +2,39 @@ import pandas as pd
 import plotly.express as px
 from sklearn.compose import make_column_transformer, ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.dummy import DummyClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
 
-class Analyzer():
+class Analyzer:
     def __init__(self):
         pass
 
-    def histogram(self, data: pd.DataFrame, column: str, target_column: str) -> None:
+    @staticmethod
+    def histogram(data: pd.DataFrame, column: str, target_column: str) -> None:
         if column not in data.columns:
             raise ValueError(f"Column '{column}' not found in data.")
-        
+
         px.histogram(data, x = column, text_auto=True, color = target_column, barmode = 'group').show()
 
-
-    def get_target_column(self, data: pd.DataFrame, target: str) -> pd.Series:
+    @staticmethod
+    def get_target_column(data: pd.DataFrame, target: str) -> pd.Series:
         if target not in data.columns:
             raise ValueError(f"Target column '{target}' not found in data.")
 
         return data[target]
-    
-    def remove_target_column(self, data: pd.DataFrame, target: str) -> pd.DataFrame:
+
+    @staticmethod
+    def remove_target_column(data: pd.DataFrame, target: str) -> pd.DataFrame:
         if target not in data.columns:
             raise ValueError(f"Target column '{target}' not found in data.")
 
         return data.drop(target, axis=1)
-    
-    def make_column_transformer(self, columns: list) -> ColumnTransformer:
+
+    @staticmethod
+    def make_column_transformer(columns: list) -> ColumnTransformer:
         one_hot = make_column_transformer((
             OneHotEncoder(drop = 'if_binary',),
             columns),
@@ -35,12 +43,48 @@ class Analyzer():
             )
         return one_hot
 
-    def fit_transform_data(self, one_hot: ColumnTransformer, data: pd.DataFrame):
+    @staticmethod
+    def fit_transform_data(one_hot: ColumnTransformer, data: pd.DataFrame):
         return one_hot.fit_transform(data)
-    
-    def dummy_target_column(self, target_column: pd.Series):
+
+    @staticmethod
+    def dummy_target_column(target_column: pd.Series):
         label_encoder = LabelEncoder()
         return label_encoder.fit_transform(target_column)
+
+    @staticmethod
+    def split_test_data(data: pd.DataFrame, target: pd.DataFrame, **kwargs):
+        return train_test_split(data, target, **kwargs)
+
+    @staticmethod
+    def get_dummy_fit(data: pd.DataFrame, target: pd.DataFrame):
+        dummy = DummyClassifier()
+        return dummy.fit(data, target)
+
+    @staticmethod
+    def get_model_score(dummy, data: pd.DataFrame, target: pd.DataFrame):
+        return dummy.score(data, target)
+
+    @staticmethod
+    def get_tree_fit(data: pd.DataFrame, target: pd.DataFrame, max_depth: int = None):
+        tree = DecisionTreeClassifier(random_state=5, max_depth=max_depth)
+        return tree.fit(data, target)
+
+    @staticmethod
+    def get_tree_predict(tree: DecisionTreeClassifier, data: pd.DataFrame):
+        return tree.predict(data)
+
+    @staticmethod
+    def get_tree_score(tree: DecisionTreeClassifier, data: pd.DataFrame, target: pd.DataFrame):
+        return tree.score(data, target)
+
+    @staticmethod
+    def plot_figure(tree: DecisionTreeClassifier, class_names: list, feature_names: list, **kwargs):
+        plt.figure(figsize = (15, 6))
+        plot_tree(tree, class_names = class_names, feature_names = feature_names, **kwargs)
+        plt.show()
+
+
 
     def dummy_dataframe_columns(self, data: pd.DataFrame, columns: pd.Index, columns_to_dummy: list) -> pd.DataFrame:
         one_hot = self.make_column_transformer(columns=columns_to_dummy)
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from exploratory_analysis import Analyzer
 
-class MLProject():
+class MLProject:
     def __init__(self):
         self.data = pd.read_csv('marketing_investimento.csv')
     
     def process(self):
         analyzer = Analyzer()
         target = analyzer.get_target_column(self.data, "aderencia_investimento")
-        data = analyzer.remove_target_column(self.data, target=target.name)
+        data = analyzer.remove_target_column(self.data, target= str(target.name))
         data = analyzer.dummy_dataframe_columns(data,
                                                 columns=data.columns,
                                                 columns_to_dummy=["estado_civil",
@@ -16,7 +16,40 @@ class MLProject():
                                                                   "inadimplencia",
                                                                   "fez_emprestimo"])
         target = analyzer.dummy_target_column(target)
-        print(target)
+        data_train, data_test, target_train, target_test = analyzer.split_test_data(data, target, stratify = target, random_state = 5)
+        dummy = analyzer.get_dummy_fit(data_train, target_train)
+        result = analyzer.get_model_score(dummy, data_test, target_test)
+        print(f"The result of the classification dummy model was {result}")
+
+        tree = analyzer.get_tree_fit(data_train, target_train)
+        analyzer.get_tree_predict(tree, data_test)
+        result = analyzer.get_tree_score(tree, data_test, target_test)
+        print(f"The result of the classification tree model was {result}")
+
+        column_names = ['casado (a)',
+                        'divorciado (a)',
+                        'solteiro (a)',
+                        'fundamental',
+                        'medio',
+                        'superior',
+                        'inadimplencia',
+                        'fez_emprestimo',
+                        'idade',
+                        'saldo',
+                        'tempo_ult_contato',
+                        'numero_contatos']
+
+        #analyzer.plot_figure(tree, class_names=['não', 'sim'], feature_names=column_names, fontsize = 1, filled = True)
+        score = analyzer.get_tree_score(tree, data_train, target_train)
+        print(f"The score of the decision tree was {score}")
+
+        tree = analyzer.get_tree_fit(data_train, target_train, max_depth=3)
+        analyzer.get_tree_predict(tree, data_test)
+        result = analyzer.get_tree_score(tree, data_test, target_test)
+        print(f"The result of the classification tree model was {result}")
+
+        score = analyzer.get_tree_score(tree, data_train, target_train)
+        print(f"The score of the decision tree was {score}")
 
 
 
